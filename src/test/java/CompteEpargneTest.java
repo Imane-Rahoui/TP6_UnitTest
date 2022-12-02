3eb828293916391b5d3f969cffbc1379ebc932a0
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-12-02 10:34:41 +0100

    comments

--- src/test/java/CompteEpargneTest.java
+++ src/test/java/CompteEpargneTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import principa.CompteEpargne;
 
 import java.util.Random;
@@ -7,6 +8,11 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompteEpargneTest {
+
+    // @BeforeEach : qbl matexe kat init bhal creation compte => setup
+    // @BeforeAll : exp creation de connexion method kate => setup
+    // @AfterEach : mn mora test d method la brit nbdl => tearDown
+    // @AfterAll : drt gae test brit nsd files ula bd => tearDownAll
 
     @BeforeEach
     void setUp() {
@@ -18,32 +24,41 @@ class CompteEpargneTest {
         System.out.println("BYE");
     }
 
+    static int i=0;
     @org.junit.jupiter.api.Test
+    @Order(3)
     void retirer() {
         System.out.println("-------RETIRER-------");
         CompteEpargne ce=new CompteEpargne("Imane",99+new Random().nextInt(),101);
         // System.out.println(ce.getSolde());
         assertEquals(100.0,ce.retirer(1));
-
+        i++;
+        System.out.println("number : "+i);
     }
 
     @org.junit.jupiter.api.Test
+    @Order(1)
     void deposer() {
         System.out.println("-------DEPOSER-------");
         CompteEpargne ce=new CompteEpargne("Imane",99+new Random().nextInt(),100);
         System.out.println(ce.getSolde());
         assertEquals(101,ce.deposer(1));
+        i++;
+        System.out.println("number : "+i);
     }
 
     @org.junit.jupiter.api.Test
+    @Order(3)
     void getSolde() {
         System.out.println("-------GET SOLDE-------");
         CompteEpargne ce=new CompteEpargne("Imane",99+new Random().nextInt(),100);
         assertEquals(100,ce.getSolde());
-
+        i++;
+        System.out.println("number : "+i);
     }
 
     @org.junit.jupiter.api.Test
+    @Order(4)
     void testUni() {
         System.out.println("-------ANOTHER TESTS-------");
 
@@ -51,5 +66,8 @@ class CompteEpargneTest {
         int t2[]={1,2,3,4};
 
         assertArrayEquals(t1,t2);
+        i++;
+        System.out.println("number : "+i);
+
     }
 }
